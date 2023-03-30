34eb60452d5597f8650e7d72cea0cb1a87ca6449
Jason Serafica <seraficaj@gmail.com>
2023-03-30 14:39:01 -0700

    prepare settings.py for deployment

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = "django_rest_api.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    default': dj_database_url.config(        # Feel free to alter this value to suit your needs.        default='postgresql://postgres:postgres@localhost:5432/mysite',        conn_max_age=600    )}
+    "default": dj_database_url.config(
+    os.environ.get("DATABASE_URL"),
+    default='postgresql://postgres:postgres@localhost:5432/mysite',
+    conn_max_age=600    
+    )
 }
 
 
